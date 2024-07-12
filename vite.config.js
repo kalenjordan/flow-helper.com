7031ee64ccc9c56541ae558c500fd88ba7e0a6eb
Kalen Jordan <kalen@kalens-laptop.lan>
2024-07-12 14:12:49 -0500

    Try again

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix({
+    serverMinify: false
+  })],
 });
